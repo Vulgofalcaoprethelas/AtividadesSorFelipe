67d5043557dc2dae9f1c42e935d257580af3fce0
Lorenzo Fernandes Da Silva <166745576+Vulgofalcaoprethelas@users.noreply.github.com>
2026-05-20 22:36:18 -0300

    Adicionando a atividade de mudança de cor do fundo

---
+++ Atividade1/script.js
@@ -0,0 +1,9 @@
+const body = document.body;
+
+const azul = () => body.style.backgroundColor = 'blue';
+const verde = () => body.style.backgroundColor = 'green';
+const vermelho = () => body.style.backgroundColor = 'red';
+
+document.getElementById('btn-azul').addEventListener('click', azul);
+document.getElementById('btn-verde').addEventListener('click', verde);
+document.getElementById('btn-vermelho').addEventListener('click', vermelho);
